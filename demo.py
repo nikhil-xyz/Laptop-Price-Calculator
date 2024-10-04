9daf12fb50b98cf3a694d5db8d92d12fbfff1f35
nikhil-xyz <sonkusarenikhil@sggs.ac.in>
2024-10-04 21:47:30 +0530

    added template, logging, exception and data ingestion module

---
+++ demo.py
@@ -0,0 +1,4 @@
+from laptop_project.pipline.training_pipeline import TrainPipeline
+
+obj = TrainPipeline()
+obj.run_pipeline()
---
+++ laptop_project/constants/__init__.py
@@ -0,0 +1,22 @@
+# Database Constants
+DATABASE_NAME = "Laptop"
+COLLECTION_NAME = "laptop_data"
+MONGODB_URI = "MONGODB_URI"
+
+PIPELINE_NAME: str = "laptop"
+ARTIFACT_DIR: str = "artifact"
+
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+FILE_NAME: str = "laptop.csv"
+MODEL_FILE_NAME = "model.pkl"
+
+"""
+Data Ingestion Constants
+"""
+DATA_INGESTION_COLLECTION_NAME:str = "laptop_data"
+DATA_INGESTION_DIR_NAME:str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR:str = "feature_store"
+DATA_INGESTION_INGESTED_DIR:str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float = 0.2
